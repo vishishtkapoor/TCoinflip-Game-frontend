8c69880883ebb2d6b8362ba4e8919aea65506020
yakkujit <yakkujit@gmail.com>
2024-09-14 09:24:31 +0900

    url

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://yakkujit.github.io/first_contract_front_end/tonconnect-manifest.json';
+const manifestUrl = 'https://https://first-contract-front-end-ten.vercel.app/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
