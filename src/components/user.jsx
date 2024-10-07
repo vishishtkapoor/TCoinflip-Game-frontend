8917c377bdb59be593c4b1825a377cd2bf3c01a2
Yakkujit <yakkujit@gmail.com>
2024-10-07 18:55:12 +0900

    userName error

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 // Get user data
 
-const User = ({userName}) => {
+const User = () => {
     return (
         <div className='pt-10 pb-4'>
             {/* Title */}
             < div className="w-full h-auto text-center bg-[inherit] text-white font-bold text-3xl leading-tight [font-family:'Poppins',Helvetica] tracking-normal" >
-                {userName}
+                User Name
             </div >
 
             {/* Profile Image and Name */}
